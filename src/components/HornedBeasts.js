6f093b681bd7f720139f8b0441a11521501499dc
Ahmad-A92-3 <ahmad.alsyed92@gmail.com>
2021-05-17 16:59:17 +0300

    solution for lab02

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,16 +1,50 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+
+
 class HornedBeasts extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            votesNo:0,           
+        }
+    }
+    counterFunction=()=>{
+        this.setState({
+            votesNo:this.state.votesNo+1
+
+        })
+
+    }
     render(){
         return(
-            <div>
-                <h2> {this.props.HornedBeastsTitle}</h2>
-                {/* <div data-src={this.props.HornedBeastsImage}>
-                </div> */}
+            <div style={{ width: '25%', display:'flex', justifyContent:'center', justifyItems:'center', flexWrap:'wrap', alignItems:'center',alighnContent:'center' } }>
+                {/* <h2> {this.props.HornedBeastsTitle}</h2>
+                
                 <img src={this.props.HornedBeastsImage} alt="image"/> 
                       
                 <p> 
                     {this.props.HornedBeastsDescription}
-                </p>
+                </p> */}
+
+                <Card style={{ width: '18rem', display:'flex' } }>
+                    <Card.Img variant="top" src={this.props.HornedBeastsImage} />
+                    <Card.Body>
+                        <Card.Title>{this.props.HornedBeastsTitle}</Card.Title>
+                        <Card.Text>
+                        {this.props.HornedBeastsDescription}
+                        
+                        </Card.Text>
+                        <Card.Text>
+                        💓  Number Of votes: {this.state.votesNo}                        
+                        </Card.Text>
+                        <Button variant="primary" onClick={this.counterFunction} >Vote</Button>
+                    </Card.Body>
+                    </Card>
     
             </div>
         )
